c65da370f8a3633f19d43df70cb0a2a0c4499871
cat9563 <crysatl9563@gmail.com>
2018-11-11 12:06:30 -0500

    started to work on class

--- pathfinder.py
+++ pathfinder.py
@@ -3,13 +3,13 @@ import random
 from PIL import ImageColor
 from PIL import Image 
 
+class Display_map:
 
-# im = Image.new('RGBA', (600, 600), 'forestGreen')
-# im.save('map.png')
+    def __selfint__(self, elevation_list, image_name, height, width):
+        pass
 
-# did list comprehsion and created a list out of that 
-with open("elevation_small.txt", 'r') as f:
-    all_elvations = [line.strip('\n').split() for line in f]
+    with open("elevation_small.txt", 'r') as f:
+        all_elvations = [line.strip('\n').split() for line in f]
     
 
 elevation_list = [[int(elevation ) for elevation in row] for row in all_elvations]
@@ -19,14 +19,6 @@ big_max = max([max(elevation) for elevation in elevation_list])
 little_min = min([min(elevation) for elevation in elevation_list])
 
 conv_to_rgb_values = [[int((elevation - little_min)/(big_max - little_min) * 255) for elevation in row] for row in elevation_list]
-
-
-
-
-
-#img = Image.new("RGB",(255,255))
-# for y, elevation in enumerate(conv_to_rgb_values):
-#   print(elevation)
 
 
 im = Image.new("RGB", (600, 600))
